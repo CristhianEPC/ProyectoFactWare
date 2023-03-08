0d26fc5ec37703a597e8e9d7a338bd333d33660b
ABYGAIL1410 <daysizhingri@gmail.com>
2023-03-08 12:06:50 -0500

    campos vacios persona

--- src/app/pages/editar-persona/editar-persona.component.ts
+++ src/app/pages/editar-persona/editar-persona.component.ts
@@ -31,6 +31,8 @@ export class EditarPersonaComponent implements OnInit {
   }
 
   Actualizar(persona: Persona) {
+    if(persona.nombre_persona!= "" && persona.apellido_persona !="" && persona.direccion_persona!="" && persona.telefono_persona!=""
+    && persona.correo_persona !="" ){
     this.service.updatePersona(persona)
       .subscribe(data => {
         this.persona = data;
@@ -45,6 +47,16 @@ export class EditarPersonaComponent implements OnInit {
         //alert("Se Actualiazo");
         this.router.navigate(['admin/crudAdmin'])
       })
+    }else{
+      Swal.fire({
+        title: 'Revise  por favor,existen campos vacios',
+        icon: 'error',
+        iconColor :'#17550c',
+        color: "#0c3255",
+        confirmButtonColor:"#0c3255",
+        background: "#63B68B",
+      })
+    }
   }
 
 }
