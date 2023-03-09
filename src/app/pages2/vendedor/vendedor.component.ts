e6e600a7c0f152a73f7b4e9526f60b0f51503ce6
CristhianEPC <cristhian.pallmay.est@tecazuay.edu.ec>
2023-03-08 21:35:40 -0500

    Mejorar el diseño en vendedor
    mensajes y cálculos de la factura

--- src/app/pages2/vendedor/vendedor.component.ts
+++ src/app/pages2/vendedor/vendedor.component.ts
@@ -26,16 +26,18 @@ export class VendedorComponent implements OnInit {
 
   //VARIABLES PARA DETALLE-FACTURA
   nuev = [];
+  ivapre: DetalleFactura2;
   idFactura;
   product: Producto;
   cantidad;
   subTotal: number;
   total: number;
-  iva: number = 0.12;
+  iva: number = 0;
 
   //variable de total final
   totalFinal: number;
   subtotaIva: number = 0;
+  subtotaCero: number = 0;
   subtotaIva12: number = 0;
 
   //VARIABLES PARA FACTURA
@@ -82,6 +84,7 @@ export class VendedorComponent implements OnInit {
     this.totalFinal = 0.00;
 
   }
+  vali: number = 1;
 
   //PARA EL CONSUMIDOR FINAL
   consumidor() {
@@ -89,7 +92,14 @@ export class VendedorComponent implements OnInit {
 
     if (document.getElementById('check').click) {
       this.consumidorfinal = "9999999999999";
-      //alert('checkbox1 esta seleccionado');
+      this.vali = this.vali + 1;
+      console.log(this.vali);
+    }
+
+    if (this.vali == 3) {
+      console.log(this.vali);
+      this.consumidorfinal = "000000000000";
+      this.vali = 1;
     }
 
   }
@@ -171,11 +181,11 @@ export class VendedorComponent implements OnInit {
       } else {
         Swal.fire({
           title: 'Productos Faltantes',
-            icon: 'warning',
-            iconColor :'#0a0a0a', //color negro
-            color: "#9e0e0e", //color rojo
-            confirmButtonColor:"#0c3255", //color azul
-            background: "#fcfcfc", //color blanco
+          icon: 'warning',
+          iconColor: '#0a0a0a', //color negro
+          color: "#9e0e0e", //color rojo
+          confirmButtonColor: "#0c3255", //color azul
+          background: "#fcfcfc", //color blanco
         })
         //alert("ya no hay productos")
       }
@@ -204,6 +214,7 @@ export class VendedorComponent implements OnInit {
     this.cantidad = this.verSeleccion;
     this.subTotal = this.product.pvp_producto;
     this.total = Math.round(this.product.pvp_producto * (+this.verSeleccion) * 100) / 100;
+    this.iva = Math.round(((this.subtotaIva) + (this.total * 0.12)) * 100) / 100;
     console.log("estoy en el total 2 " + this.total)
 
     this.nuev = [this.idFactura, this.product, this.verSeleccion, this.subTotal]
@@ -233,76 +244,199 @@ export class VendedorComponent implements OnInit {
         this.totalFinal = Math.round((this.totalFinal + this.total) * 100) / 100;
         if (this.product.constIva == true) {
           this.subtotaIva = Math.round(((this.subtotaIva) + (this.total * 0.12)) * 100) / 100;
-
+          this.subtotaIva12 = Math.round((this.subtotaIva12 + ((this.total * 0.12) + this.total)) * 100) / 100;
+          this.totalAPagar = Math.round((this.subtotaIva12 + this.subtotaCero) * 100) / 100;
+        } else {
+          this.subtotaCero = Math.round((this.subtotaCero + this.total) * 100) / 100;
+          this.totalAPagar = Math.round((this.subtotaCero + this.subtotaIva12) * 100) / 100;
         }
-        this.totalAPagar = Math.round((this.subtotaIva + this.totalFinal) * 100) / 100;
-        this.subtotaIva12 = Math.round((this.subtotaIva + this.totalFinal) * 100) / 100;
+
+
         this.listarFactura();
         this.listarDeta();
         console.log("creado")
 
       })
-    // } else {
-    //Poner el mensaje
-    // alert("ya no hay productos")
-    //}
-
-    //} else {
-    //Poner el mensaje
-    // alert("Ingrese los datos de la factura")
-    // }
 
 
   }
 
   // METODO PARA GUARDAR UNA FACTURA
   factuNueva: Factura2;
+  consumidorFinal: Persona;
 
   guardar(fact: Factura2) {
-    this.factuNueva = {
-      "numeroFact": this.numfac,
-      "fecha": fact.fecha,
-      "persona": fact.persona,
-      "id_factura": 0
+
+    //TRAER EL CONSUMIDOR FINAL
+    this.personaService.getPersonaId(1).
+      subscribe(data => {
+        this.consumidorFinal = data;
+        console.log(data.apellido_persona + " " + data.nombre_persona);
+      })
+
+    //VALIDACION PARA FACTURA
+    if (fact.fecha != null) {
+
+      if (this.fac.persona != undefined || this.fac.persona != null || this.vali != 1) {
+
+        //VALIDACION DE CONSUMIDOR FINAL
+        if (this.vali == 2) {
+
+          Swal.fire({
+            title: '¿Asignar factura con consumidor final?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'SI',
+            denyButtonText: `NO`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              //PONER EL CODIGO A EJECUTAR
+              this.factuNueva = {
+                "numeroFact": this.numfac,
+                "fecha": fact.fecha,
+                "persona": this.consumidorFinal,
+                "id_factura": 0
+              }
+
+              this.factService.create(this.factuNueva)
+                .subscribe(data => {
+                  this.fact2 = data;
+                  Swal.fire({
+                    title: 'Factura Asignada con Consumidor F.',
+                    icon: 'success',
+                    iconColor: '#17550c',
+                    color: "#0c3255",
+                    confirmButtonColor: "#0c3255",
+                    background: "#63B68B",
+                  })
+
+                  //console.log("creado factura");
+                })
+
+              //this.nuevaFactura();
+
+              //FIN DEL CODIGO A EJECUTAR
+              //Swal.fire('Listo!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Desabilite el check', '', 'info')
+            }
+          })
+
+        } else {
+          this.factuNueva = {
+            "numeroFact": this.numfac,
+            "fecha": fact.fecha,
+            "persona": fact.persona,
+            "id_factura": 0
+          }
+
+          this.factService.create(this.factuNueva)
+                .subscribe(data => {
+                  this.fact2 = data;
+                  Swal.fire({
+                    title: 'Factura Asignada con Persona',
+                    icon: 'success',
+                    iconColor: '#17550c',
+                    color: "#0c3255",
+                    confirmButtonColor: "#0c3255",
+                    background: "#63B68B",
+                  })
+
+                  //console.log("creado factura");
+                })
+
+          //this.nuevaFactura();
+        }
+
+      } else {
+        Swal.fire({
+          title: 'Campos de llenar en factura',
+          icon: 'warning',
+          iconColor: '#0a0a0a', //color negro
+          color: "#9e0e0e", //color rojo
+          confirmButtonColor: "#0c3255", //color azul
+          background: "#fcfcfc", //color blanco
+        })
+      }
+    } else {
+
+      Swal.fire({
+        title: 'Campos de llenar en factura',
+        icon: 'warning',
+        iconColor: '#0a0a0a', //color negro
+        color: "#9e0e0e", //color rojo
+        confirmButtonColor: "#0c3255", //color azul
+        background: "#fcfcfc", //color blanco
+      })
     }
 
-    this.factService.create(this.factuNueva)
-      .subscribe(data => {
-        this.fact2 = data;
-        Swal.fire({
-          title: 'Factura Asignada',
-          icon: 'success',
-          iconColor: '#17550c',
-          color: "#0c3255",
-          confirmButtonColor: "#0c3255",
-          background: "#63B68B",
-        })
-        //console.log("creado factura");
-      })
+
+
+
+
+
+    console.log("---- " + this.vali)
+    //console.log("claro "+document.getElementById("idpersona").textContent.toString())
+
+  }
+
+  nuevaFactura() {
+
+
   }
 
 
   //METODO PARA ELIMINAR EL DETALLE AL QUITAR
   eliminar(detal: DetalleFactura2): void {
-    if (confirm('¿Seguro deseas quitar este Producto?')) {
-      this.detalleService.eliminarDetalle(detal)
-        .subscribe(data => {
+    Swal.fire({
+      title: 'Esta Seguro?',
+      text: "No será capaz de revertirlo!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Borrarlo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //AQUI COLOCAR EL CÓDIGO
+        this.detalleService.eliminarDetalle(detal)
+          .subscribe(data => {
+            //this.ivapre = detal;
+            this.detalleService.resutarar(detal).subscribe(data => {
+              console.log("se restauro en el stock")
+            })
 
-          this.detalleService.resutarar(detal).subscribe(data => {
-            console.log("se restauro en el stock")
+            this.totalFinal = Math.round((this.totalFinal - detal.total) * 100) / 100;
+            //console.log("gggg " +detal.producto.constIva);
+            if (detal.producto.constIva == true) {
+              this.subtotaIva = Math.round(((this.subtotaIva) - (detal.total * 0.12)) * 100) / 100;
+              this.subtotaIva12 = Math.round((this.subtotaIva12 - ((detal.total * 0.12) + detal.total)) * 100) / 100;
+              this.totalAPagar = Math.round((this.totalAPagar - ((detal.total * 0.12) + detal.total)) * 100) / 100;
+            } else {
+              this.subtotaCero = Math.round((this.subtotaCero - detal.total) * 100) / 100;
+              this.totalAPagar = Math.round((this.totalAPagar - detal.total) * 100) / 100;
+            }
+
+            //this.totalAPagar = Math.round((this.subtotaIva + this.totalFinal) * 100) / 100;
+
+            this.listarDeta();
+            //this.facN = this.facN.filter(p => p !== detal);
           })
+        //FIN DEL BLOQUE DE CÓDIGO
+        Swal.fire(
+          'Borrado!',
+          'Su archivo ha sido borrado.',
+          'success'
 
-          this.totalFinal = Math.round((this.totalFinal - detal.total) * 100) / 100;
-          if (this.product.constIva == true) {
-            this.subtotaIva = Math.round(((this.subtotaIva) - (this.total * 0.12)) * 100) / 100;
+        )
+      }
+    })
 
-          }
-          this.totalAPagar = Math.round((this.subtotaIva + this.totalFinal) * 100) / 100;
-          this.subtotaIva12 = Math.round((this.subtotaIva + this.totalFinal) * 100) / 100;
-          this.listarDeta();
-          //this.facN = this.facN.filter(p => p !== detal);
-        })
-    }
+
+    // if (confirm('¿Seguro deseas quitar este Producto?')) {
+
+    // }
 
     //this.listarDeta();
 
@@ -310,18 +444,38 @@ export class VendedorComponent implements OnInit {
 
   //PARA RECAGAR LA PAGINA Y GENERAR LA FACTURA
   generar() {
-    if (confirm("¿Generar Factura?")) {
-      Swal.fire({
-        title: 'Factura Generada éxitosamente',
-        icon: 'success',
-        iconColor: '#17550c',
-        color: "#0c3255",
-        confirmButtonColor: "#0c3255",
-        background: "#63B68B",
-      })
-      //this.router.navigate(['vendedor/vendedor']);
-      window.location.reload();
-    }
+
+    Swal.fire({
+      title: 'Generar la Factura?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Generar',
+      denyButtonText: `No Generar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        //PONER EL CODIGO A EJECUTAR
+        window.location.reload();
+        //FIN DEL CODIGO A EJECUTAR
+        Swal.fire('Listo!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Cancelado', '', 'info')
+      }
+    })
+
+
+    // if (confirm("¿Generar Factura?")) {
+    //   Swal.fire({
+    //     title: 'Factura Generada éxitosamente',
+    //     icon: 'success',
+    //     iconColor: '#17550c',
+    //     color: "#0c3255",
+    //     confirmButtonColor: "#0c3255",
+    //     background: "#63B68B",
+    //   })
+    //   //this.router.navigate(['vendedor/vendedor']);
+
+    // }
 
   }
 
