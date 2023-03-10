9f433b251bd889aaa09ee9aeb15756690f25d2ba
ABYGAIL1410 <daysizhingri@gmail.com>
2023-03-09 20:05:33 -0500

    Revert "Merge branch 'abigail01' into abigail02"
    This reverts commit ea40750e5ea5e2a5bc7a9ad5272703c0374048a4, reversing
    changes made to 2c0034475b578ffa278a42148a26b43300049858.

--- src/app/pages/registro-persona/registro-administrador.component.ts
+++ src/app/pages/registro-persona/registro-administrador.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Persona } from 'src/app/modelo/Persona';
 import { PersonaService } from 'src/app/servicios/api/persona.service';
 import Swal from 'sweetalert2';
-import { FormBuilder, Validators ,FormGroup} from '@angular/forms';
+
 @Component({
   selector: 'app-registro-administrador',
   templateUrl: './registro-administrador.component.html',
@@ -12,40 +12,37 @@ import { FormBuilder, Validators ,FormGroup} from '@angular/forms';
 export class RegistroAdministradorComponent implements OnInit {
 
   persona = new Persona();
-  myForm:FormGroup;
-  constructor(private router:Router, private service:PersonaService , public fb:FormBuilder) { 
-    this.myForm = fb.group({
-      cedula: ['', Validators.required],
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      correo: ['', Validators.required],
-      telefono: ['', Validators.required],
-      direccion: ['', Validators.required]
-    })
-  }
+
+  constructor(private router: Router, private service: PersonaService) { }
 
   ngOnInit(): void {
   }
 
-  guardar(persona:Persona){
-    if (this.myForm.valid){
-    this.service.create(persona)
-    .subscribe(data=>{
-      Swal.fire({
-        title: 'Persona Guardado éxitosamente',
-        icon: 'success',
-        iconColor :'#17550c',
-        color: "#0c3255",
-        confirmButtonColor:"#0c3255",
-        background: "#63B68B",
-      })
-      //alert("Se agrego con exito..!");
-      this.router.navigate(['admin/crudAdmin']);
-    })
-  }else{
-    alert('Revise que los campos esten llenados correctamente')
-  }
-  
+  guardar(persona: Persona) {
+
+    if (persona.cedula != "" && persona.nombre_persona != ""
+      && persona.apellido_persona != "" && persona.direccion_persona != ""
+      && persona.telefono_persona != "" && persona.correo_persona != "") {
+
+      this.service.create(persona)
+        .subscribe(data => {
+          Swal.fire({
+            title: 'Persona Guardado éxitosamente',
+            icon: 'success',
+            iconColor: '#17550c',
+            color: "#0c3255",
+            confirmButtonColor: "#0c3255",
+            background: "#63B68B",
+          })
+          //alert("Se agrego con exito..!");
+          this.router.navigate(['admin/crudAdmin']);
+        })
+
+    } else {
+      Swal.fire('Llene todos los campos', '', 'info')
+    }
+
+
   }
 
 }
